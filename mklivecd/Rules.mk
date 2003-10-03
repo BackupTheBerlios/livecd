11c7fa73ea1597b7aca1b9bec20cc3236e25045f
jaco <jaco>
2003-10-03 06:19:06 +0000

    Changed keyb=language to keyb=mapping

--- mklivecd/Rules.mk
+++ mklivecd/Rules.mk
@@ -1,8 +1,20 @@
-# $Id: Rules.mk,v 1.5 2003/10/02 07:57:55 jaco Exp $
+# $Id: Rules.mk,v 1.6 2003/10/03 06:19:06 jaco Exp $
 
 # User parameters: These are parameters that can be changed for
 # your installation
-DEF_KEYBOARD=en_US
+# Valid options:
+#     al am am_old am_phonetic ar az
+#     be ben bg bg_phonetic br bs by
+#     ch_de ch_fr cz cz_qwerty
+#     de de_nodeadkeys dev dk dvorak dvorak_no dvorak_se
+#     ee es fi fr ge_la ge_ru gr gr_pl guj gur hr hu
+#     ie il il_phonetic ir is it iu jp kan kr
+#     la lao lt lt_b lt_new lt_p lv mal mk mm mng mt mt_us
+#     nl no ori pl pl2 pt qc ro ro2 ru ru_yawerty
+#     sapmi sapmi_sefi se si sk sk_qwerty sr syr syr_p
+#     tel th tj tml tr_f tr_q tscii
+#     ua uk us us_intl uz vn yu
+DEF_KEYBOARD=us
 
 # Version identifiers: These should only be changed by the release
 # manager as part of making a new release
