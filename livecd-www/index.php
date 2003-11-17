c01256f08264679152228c5f97926296812864bd
jaco <jaco>
2003-11-17 13:20:49 +0000

    Initial revision

---
+++ livecd-www/index.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title>LiveCD - The Linux LiveCD project</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body bgcolor="#FFFFFF" marginheight="10" marginwidth="10" link=#0050a0 vlink=#0050a0 alink=#000000>
+<center>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr><td bgcolor="#000000">
+		<table width="100%" border="0" cellspacing="2" cellpadding="0">
+			<tr><td bgcolor=#305080>
+				<font color=#ffffff size=+4><b>&nbsp;L&nbsp;i&nbsp;v&nbsp;e&nbsp;</b></font>
+			</td>
+			<td width=100% bgcolor=#d0d0d0>
+				<font size=+4><b>&nbsp;C&nbsp;D</b></font>
+			</td></tr>
+			<tr><td bgcolor=#000000 colspan=2>
+				<font color=#808080><small><b>
+				&nbsp;&nbsp;
+				<font color=#ffffff>h o m e</font>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<!--<a href="about.php"><font color=#c0c0c0>a b o u t</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;-->
+				<a href="http://www.linuxminicd.org/mklivecd/downloads/"><font color=#c0c0c0>d o w n l o a d</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="links.php"><font color=#c0c0c0>l i n k s</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="http://developer.berlios.de/projects/livecd/"><font color=#c0c0c0>p r o j e c t</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="http://developer.berlios.de/bugs/?group_id=1149"><font color=#c0c0c0>b u g s</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="http://developer.berlios.de/cvs/?group_id=1149"><font color=#c0c0c0>c v s</font></a>
+				</b></small></font>
+			</td></tr>
+		</table>
+	</td></tr>
+	<tr><td valign="top" colspan="2">
+		<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			<tr><td height="10"></td></tr>
+			<tr><td valign="top">
+				<table width="100%" border="0" cellspacing="0" cellpadding="0">
+					<tr><td bgcolor="#000000">
+						<table width=100% border=0 cellspacing=2 cellpadding=5 cols=2>
+							<tr><td bgcolor=#305080 nowrap>
+								<b><font color=#ffffff>m o n &nbsp; n o v &nbsp; 1 7 &nbsp; 2 0 0 3 &nbsp;</font></b>
+							</td>
+							<td bgcolor=#d0d0d0 nowrap>
+								<b>&nbsp;</b>
+							</td></tr>
+							<tr><td colspan=2 bgcolor=#d0d0d0>
+								<b>l a u n c h , &nbsp; a n d &nbsp; a g a i n</b><br>
+								What better way to celebrate actually having a page up on our new home, than to have the first mainstream version of a Linux LiveCD made using mklivecd released to the masses. That is right, Texstar of <a href="http://www.pclinuxonline.com">PCLinuxOnline</a> has made preview 3 of PCLinuxOS available for everybody to try. More details, including download locations, can be found <a href="http://www.pclinuxonline.com/modules.php?name=Forums&file=viewtopic&topic=2368&forum=44">here</a> or on our <a href="links.html">links page</a>.
+							</td></tr>
+						</table>
+					</td></tr>
+				</table>
+			</td>
+			<td width="10"></td>
+			<td width="300" valign="top">
+				<table width="100%" border="0" cellspacing="0" cellpadding="0">
+					<tr><td bgcolor="#000000">
+						<table width=100% border=0 cellspacing=2 cellpadding=5 cols=2>
+							<tr><td bgcolor=#305080 nowrap>
+								<b><font color=#ffffff>s t a t i s t i c s &nbsp;</font></b>
+							</td>
+							<td bgcolor=#d0d0d0 nowrap>
+								<b>&nbsp;</b>
+							</td></tr>
+							<tr><td colspan=2 bgcolor=#d0d0d0>
+								<?php include('/home/users/jaco/livecd-stats.cache'); ?>
+							</td></tr>
+						</table>
+					</td></tr>
+				</table>
+			</td></tr>
+		</table>
+	</td></tr>
+</table>
+</center>
+</body>
---
+++ livecd-www/links.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+<head>
+	<title>LiveCD - The Linux LiveCD project</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body bgcolor="#FFFFFF" marginheight="10" marginwidth="10" link=#0050a0 vlink=#0050a0 alink=#000000>
+<center>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr><td bgcolor="#000000">
+		<table width="100%" border="0" cellspacing="2" cellpadding="0">
+			<tr><td bgcolor=#305080>
+				<font color=#ffffff size=+4><b>&nbsp;L&nbsp;i&nbsp;v&nbsp;e&nbsp;</b></font>
+			</td>
+			<td width=100% bgcolor=#d0d0d0>
+				<font size=+4><b>&nbsp;C&nbsp;D</b></font>
+			</td></tr>
+			<tr><td bgcolor=#000000 colspan=2>
+				<font color=#808080><small><b>
+				&nbsp;&nbsp;
+				<a href="index.php"><font color=#c0c0c0>h o m e</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<!--<a href="about.php"><font color=#c0c0c0>a b o u t</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;-->
+				<a href="http://www.linuxminicd.org/mklivecd/downloads/"><font color=#c0c0c0>d o w n l o a d</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<font color=#ffffff>l i n k s</font>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="http://developer.berlios.de/projects/livecd/"><font color=#c0c0c0>p r o j e c t</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="http://developer.berlios.de/bugs/?group_id=1149"><font color=#c0c0c0>b u g s</font></a>
+				&nbsp;&nbsp;|&nbsp;&nbsp;
+				<a href="http://developer.berlios.de/cvs/?group_id=1149"><font color=#c0c0c0>c v s</font></a>
+				</b></small></font>
+			</td></tr>
+		</table>
+	</td></tr>
+	<tr><td valign="top" colspan="2">
+		<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			<tr><td height="10"></td></tr>
+			<tr><td bgcolor="#000000">
+				<table width=100% border=0 cellspacing=2 cellpadding=5 cols=2>
+					<tr><td bgcolor=#305080 nowrap>
+						<b><font color=#ffffff>s u n &nbsp; n o v &nbsp; 1 6 &nbsp; 2 0 0 3 &nbsp; . &nbsp; P C L i n u x O S &nbsp; p r e v i e w &nbsp; 3 &nbsp;</font></b>
+					</td>
+					<td bgcolor=#d0d0d0 nowrap>
+						<b>&nbsp;</b>
+					</td></tr>
+					<tr><td colspan=2 bgcolor=#d0d0d0>
+						<b>i n f o</b><br>
+						PCLinuxOS 2K4 Preview 3 is a live knoppix style cd based on Mandrake 9.2 that runs entirely from a bootable CD. Data on the CD is uncompressed on the fly, allowing up to 2 GB worth of system and programs on one CD, including a complete X server, KDE 3.1.4 and Gnome 2.4, and large packages like OpenOffice 1.1final and Mozila 1.5. Since it runs solely off the CD, PCLinuxOS makes an excellent portable Linux demo or system rescue disk, but its completeness makes it a good general purpose desktop as well. PCLinuxOS should work on most modern computer hardware. Recommended memory to run is 256mb or more.<p>
+						<b>d i s c u s s i o n</b><br>
+						<a href="http://www.pclinuxonline.com/modules.php?name=Forums&file=viewtopic&topic=2368&forum=44">http://www.pclinuxonline.com/modules.php?name=Forums&file=viewtopic&topic=2368&forum=44</a><p>
+						<b>m i r r o r s</b><br>
+						<a href="ftp://ftp.ibiblio.org/pub/Linux/distributions/contrib/texstar/pclinuxos/live-cd/english/preview-3">Mirror 1 (ftp.ibiblio.org)</a><br>
+<a href="ftp://ftp.nluug.nl/pub/metalab/distributions/contrib/texstar/pclinuxos/live-cd/english/preview-3/">Mirror 2 (ftp.nluug.nl)</a><br>
+<a href="ftp://ftp.gwdg.de/pub/linux/mirrors/sunsite/distributions/contrib/texstar/pclinuxos/live-cd/english/preview-3/">Mirror 3 (ftp.gwdg.de)</a>
+					</td></tr>
+				</table>
+			</td></tr>
+		</table>
+	</td></tr>
+</table>
+</center>
+</body>
+</html>
