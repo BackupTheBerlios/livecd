d92687fb4a43183ac5c0c10d381034103928a73c
jaco <jaco>
2003-12-30 20:19:51 +0000

    Holiday update :)

--- mklivecd/Rules.mk
+++ mklivecd/Rules.mk
@@ -1,12 +1,30 @@
-# $Id: Rules.mk,v 1.21 2003/12/07 17:25:08 jaco Exp $
+# $Id: Rules.mk,v 1.22 2003/12/30 20:19:51 jaco Exp $
 
 # User parameters: These are parameters that can be changed for
 # your installation (See the FAQ for valid options)
+# The preferred way to change/overeride this is with a
+#     make DEF_KEYBOARD=dvorak
 DEF_KEYBOARD=us
 
 # these two parameters need to match up to allow splash to work
+# The preferred way to change/override this is with a
+#     make DEF_RESOLUTION=800x600 DEF_VGAMODE=788
 DEF_RESOLUTION=1024x768
 DEF_VGAMODE=791
+
+# splash progress bar parameters
+# MAX_SPLASH: default: 65534. With a patched bootsplash, the
+# preferred way to change/override this is with a
+#     make MAX_SPLASH=32767
+MAX_SPLASH=65534
+
+### no more user-editable parameters after this line
+
+# various script number of splash steps
+VAL_SPLASH_LINUXRC=5
+VAL_SPLASH_SYSINIT=5
+VAL_SPLASH_HWDETECT=17
+VAL_SPLASH_FULL=27
 
 # Version identifiers: These should only be changed by the release
 # manager as part of making a new release
@@ -71,7 +89,9 @@ DOCDIST=\
 	COPYING \
 	FAQ \
 	README \
-	TODO \
+	TODO
+
+BUILDDIST=\
 	Makefile \
 	Rules.mk \
 	Modules.mk \
