ce027b851b1f98ca100f959d1c3511c5a332e69d
jaco <jaco>
2004-01-05 09:30:35 +0000

    Removed hardcoded .o module extension for needed modules, 2.6 has .ko (Start of 2.6 support)

--- mklivecd/Modules.mk
+++ mklivecd/Modules.mk
@@ -1,79 +1,79 @@
-# $Id: Modules.mk,v 1.4 2003/12/19 12:53:30 jaco Exp $
+# $Id: Modules.mk,v 1.5 2004/01/05 09:30:36 jaco Exp $
 
 # these are base modules to be used for the fs types, IDE and
 # their dependancies
 DEF_MODULES_PATH=\
-	fs/ext3/ext3.o \
-	fs/jbd/jbd.o \
-	fs/nls/nls_iso8859-13.o \
-	fs/nls/nls_iso8859-14.o \
-	fs/nls/nls_iso8859-15.o \
-	fs/nls/nls_iso8859-1.o \
-	fs/nls/nls_iso8859-2.o \
-	fs/nls/nls_iso8859-3.o \
-	fs/nls/nls_iso8859-4.o \
-	fs/nls/nls_iso8859-5.o \
-	fs/nls/nls_iso8859-6.o \
-	fs/nls/nls_iso8859-7.o \
-	fs/nls/nls_iso8859-8.o \
-	fs/nls/nls_iso8859-9.o \
-	fs/nls/nls_utf8.o
+	fs/ext3/ext3 \
+	fs/jbd/jbd \
+	fs/nls/nls_iso8859-13 \
+	fs/nls/nls_iso8859-14 \
+	fs/nls/nls_iso8859-15 \
+	fs/nls/nls_iso8859-1 \
+	fs/nls/nls_iso8859-2 \
+	fs/nls/nls_iso8859-3 \
+	fs/nls/nls_iso8859-4 \
+	fs/nls/nls_iso8859-5 \
+	fs/nls/nls_iso8859-6 \
+	fs/nls/nls_iso8859-7 \
+	fs/nls/nls_iso8859-8 \
+	fs/nls/nls_iso8859-9 \
+	fs/nls/nls_utf8
 
 # these are the dependancies for the above
 DEF_MODULES_OPT_PATH=\
-	lib/zlib_inflate/zlib_inflate.o
+	lib/zlib_inflate/zlib_inflate
 
 DEF_MODULES_DEP_PATH= \
-	fs/isofs/isofs.o \
-	drivers/cdrom/cdrom.o \
-	drivers/ide/ide-cd.o \
-	drivers/misc/aes.o \
-	drivers/block/loop.o
+	fs/isofs/isofs \
+	drivers/cdrom/cdrom \
+	drivers/ide/ide-cd \
+	drivers/misc/aes \
+	drivers/block/loop
 
 # these modules are for all the SCSI types out there
 # not available: ncr53c8xx
-# removed: drivers/scsi/aic7xxx_old.o (two modules seems to hang Adaptec controllers)
+# removed: drivers/scsi/aic7xxx_old (two modules seems to hang Adaptec controllers)
 DEF_SCSI_MODULES_PATH= \
-	drivers/scsi/aic7xxx/aic7xxx.o \
-	drivers/scsi/BusLogic.o \
-	drivers/scsi/NCR53c406a.o \
-	drivers/scsi/initio.o \
-	drivers/scsi/advansys.o \
-	drivers/scsi/aha1740.o \
-	drivers/scsi/aha1542.o \
-	drivers/scsi/aha152x.o \
-	drivers/scsi/atp870u.o \
-	drivers/scsi/dtc.o \
-	drivers/scsi/eata.o \
-	drivers/scsi/fdomain.o \
-	drivers/scsi/gdth.o \
-	drivers/scsi/megaraid.o \
-	drivers/scsi/pas16.o \
-	drivers/scsi/pci2220i.o \
-	drivers/scsi/pci2000.o \
-	drivers/scsi/psi240i.o \
-	drivers/scsi/qlogicfas.o \
-	drivers/scsi/qlogicfc.o \
-	drivers/scsi/qlogicisp.o \
-	drivers/scsi/seagate.o \
-	drivers/scsi/t128.o \
-	drivers/scsi/tmscsim.o \
-	drivers/scsi/u14-34f.o \
-	drivers/scsi/ultrastor.o \
-	drivers/scsi/wd7000.o \
-	drivers/scsi/a100u2w.o \
-	drivers/scsi/3w-xxxx.o
+	drivers/scsi/aic7xxx/aic7xxx \
+	drivers/scsi/BusLogic \
+	drivers/scsi/NCR53c406a \
+	drivers/scsi/initio \
+	drivers/scsi/advansys \
+	drivers/scsi/aha1740 \
+	drivers/scsi/aha1542 \
+	drivers/scsi/aha152x \
+	drivers/scsi/atp870u \
+	drivers/scsi/dtc \
+	drivers/scsi/eata \
+	drivers/scsi/fdomain \
+	drivers/scsi/gdth \
+	drivers/scsi/megaraid \
+	drivers/scsi/pas16 \
+	drivers/scsi/pci2220i \
+	drivers/scsi/pci2000 \
+	drivers/scsi/psi240i \
+	drivers/scsi/qlogicfas \
+	drivers/scsi/qlogicfc \
+	drivers/scsi/qlogicisp \
+	drivers/scsi/seagate \
+	drivers/scsi/t128 \
+	drivers/scsi/tmscsim \
+	drivers/scsi/u14-34f \
+	drivers/scsi/ultrastor \
+	drivers/scsi/wd7000 \
+	drivers/scsi/a100u2w \
+	drivers/scsi/3w-xxxx
 
 # depending on kernel version, these have different names
 DEF_SCSI_MODULES_OPT_PATH= \
-	drivers/scsi/sym53c8xx_2/sym53c8xx.o \
-	drivers/scsi/sym53c8xx_2/sym53c8xx_2.o
+	drivers/scsi/sym53c8xx_2/sym53c8xx \
+	drivers/scsi/sym53c8xx_2/sym53c8xx_2
 
 # these are dependancies for the above modules
 DEF_SCSI_MODULES_DEP_PATH=\
-	drivers/scsi/scsi_mod.o \
-	drivers/scsi/sr_mod.o \
-	drivers/scsi/sd_mod.o
+	drivers/scsi/scsi_mod \
+	drivers/scsi/sr_mod \
+	drivers/scsi/sd_mod
 
 # convert our module paths to their basenames
 DEF_MODULES=$(shell for m in $(DEF_MODULES_PATH); do basename $$m; done)
