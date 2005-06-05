07dad8ebec5cc9c2694f66ebe5c1a2df36e4d751
ikerekes <ikerekes>
2005-06-05 02:57:49 +0000

    Moved back cdrom as module - Thanks Darin for testing it

--- mklivecd/Modules.mk
+++ mklivecd/Modules.mk
@@ -1,4 +1,4 @@
-# $Id: Modules.mk,v 1.22 2005/03/18 19:10:03 tom_kelly33 Exp $
+# $Id: Modules.mk,v 1.23 2005/06/05 02:57:49 ikerekes Exp $
 
 # these are base modules to be used for the fs types, IDE and
 # their dependancies
@@ -52,9 +52,9 @@ DEF_MODULES_DEP_PATH_24=\
 
 DEF_MODULES_DEP_PATH_26=\
 	fs/isofs/isofs \
+	drivers/cdrom/cdrom \
 	drivers/ide/ide-cd \
-	drivers/block/loop \
-#	drivers/cdrom/cdrom
+	drivers/block/loop 
 
 # these modules are for all the SCSI types out there
 # not available: ncr53c8xx
